e9627c9703d4a9243b6feb2c92dffc7588a2251e
Ricky Aston <raston3232@gmail.com>
2021-08-03 15:00:46 +0700

    update ui login dan regis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.regislog')
 
-@section('content')
+@section('regislog')
 <!--wrapper-->
-<div class="container">
+<div class="regislog">
     <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
-        <div class="container-fluid">
-            <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-                <div class="col mx-auto">
-                    <div class="mb-4 text-center">
-                        <img src="{{asset('images/uns-biru.png')}}" width="360" alt="" />
-                    </div>
-                    <div class="card">
-                        <div class="card-header text-center"><h1><h1></div>
+        <div class="mb-4 text-center">
+            <img src="{{asset('images/login-images/login.svg')}}" width="360" alt="" class="regislog-img"/>
+        </div>
+        <div class="card-body p-5">
+            <div><h1 class="regislog-title">Welcome Back!<h1></div>
                             <div class="card-body">
-
-                                <div class="text-center"><i class="bx bxs-user-circle text-primary font-50"></i><h4 class="form-label ">Masuk </h4>
-                                </div>
 
                                 <div class="login-separater text-center mb-4"> 
                                     <hr/>
@@ -25,8 +19,8 @@
                                     <form class="row g-3" method="POST" action="{{ route('login') }}">           
                                     @csrf
                                         <div class="col-12">
-                                            <label for="inputEmailAddress" class="form-label">Email :</label>
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                            <label for="inputEmailAddress" class="form-label regislog-text">Email :</label>
+                                            <input id="email" type="email" class="form-control regislog-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -35,9 +29,9 @@
                                         </div>
 
                                         <div class="col-12">
-                                            <label for="inputChoosePassword" class="form-label">Password :</label>
+                                            <label for="inputChoosePassword" class="form-label regislog-text">Password :</label>
                                             <div class="input-group" id="show_hide_password">
-                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                                <input id="password" type="password" class="form-control regislog-input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                                 @error('password')
                                                     <span class="invalid-feedback" role="alert">
                                                         <strong>{{ $message }}</strong>
@@ -48,21 +42,16 @@
 
                                         <div class="col-12">
                                             <div class="d-grid">
-                                                <button type="submit" class="btn btn-primary btn-lg px-5">Masuk</button>
+                                                <button type="submit" class="btn btn-primary btn-lg px-5 regislog-button">Masuk</button>
                                             </div>
                                         </div>
 
                                         <div class="col-12"> 
                                             <div class="d-grid">
-                                                <a href="{{url('/register')}}" class="btn btn-primary btn-lg px-5 text-center" >Daftar Baru</a>
+                                                <a href="{{url('/register')}}" class="btn btn-primary btn-lg px-5 text-center regislog-button" >Daftar Baru</a>
                                             </div>
                                         </div>
                                     </form>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.regislog')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-xl-7 mx-auto">
-            <div class="mb-4 text-center">
-                <img src="{{asset('images/uns-biru.png')}}" width="360" alt="" />
-            </div>
-            <div class="card">
-                <div class="card-header text-center"><h1><h1></div>
-                    <div class="card-body p-5">
-                        <div class="card-title d-flex align-items-center">
-                            <div><i class="bx bxs-user me-1 font-22 text-primary"></i>
-                            </div>
-                            <h5 class="mb-0 text-primary">User Registration</h5>
-                        </div>
-                        <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                            <div class="col-md-12">
-                                <label for="inputFirstName" class="form-label">Nama</label>
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+@section('regislog')    
+<div class="regislog" >
+    <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+        <div class="mb-4 text-center">
+            <img src="{{asset('images/login-images/registrasi.svg')}}"  width="360" alt="" class="regislog-img"/>
+        </div>
+        <div class="card-body p-5">
+            <div><h1 class="regislog-title">Welcome!</h1></div>
+            <div class="card-body">
+
+                <div class="login-separater text-center mb-4"> 
+                    <hr/>
+                </div>
+            
+            <div class="form-body">
+            <form class="row g-3" method="POST" action="{{ route('register') }}">
+            @csrf
+                <div class="col-12">
+                    <label for="inputFirstName" class="form-label regislog-text">Nama Lengkap</label>
+                        <input id="name" type="text" class="form-control regislog-input @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                            <div class="col-md-12">
-                                <label for="inputEmail" class="form-label">Email</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                 <div class="col-12">
+                    <label for="inputEmail" class="form-label">Email</label>
+                        <input id="email" type="email"class="form-control regislog-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
                             </div>
-                            <div class="col-md-12">
-                                <label for="inputPassword" class="form-label">Password</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                <div class="col-12">
+                    <label for="inputPassword" class="form-label">Password</label>
+                        <input id="password" type="password" class="form-control regislog-input @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                            <div class="col-md-12">
-                                <label for="inputPassword" class="form-label">Confirm Password</label>
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+
+                <div class="col-12">
+                    <label for="inputPassword" class="form-label">Confirm Password</label>
+                        <input id="password-confirm" type="password" class="form-control regislog-input" name="password_confirmation" required autocomplete="new-password">
                             </div>
-                            <br>
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-primary px-5">Register</button>
+                            
+                <div class="col-12">
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary px-5 regislog-button">REGISTER</button>
+                    </div>
+                </div>
+                            
+                                
                             </div>
                         </form>
                     </div>
-                </div>
-            </div>
-        </div>
+                    </div>
+                    </div>
+    </div>
     </div>
 </div>
 @endsection
